df612231860456b7a17391d0ae2ceb3aa7e41da3
Memo <gbastoa17@gmail.com>
2022-12-12 00:59:31 -0600

    se completaron los ejercicios

--- ejerciciosSegundaParte/js/main.js
+++ ejerciciosSegundaParte/js/main.js
@@ -41,3 +41,77 @@ btnCalculaMenor.addEventListener("click", function (event){
     
     resultado.innerHTML += "<br> El numero menor es: " + encuentraMenor(arreglo1);
 });
+
+///////////// Funcion para el ejercicio 4 //////////////////
+function multiplo3(numero) {
+    if (numero%3 ==0){
+        return "<br> Si es multiplo de 3"
+    }else {
+        return "<br> No es multiplo de 3"
+    }
+}
+
+let btnMultiplo = document.getElementById("btnMultiplo");
+btnMultiplo.addEventListener("click", function (event){
+    event.preventDefault();  //le quita el funcionamiento que tenía el boton por default
+    let n1 = parseInt(document.getElementById("multiplo").value);
+    let resultado = document.getElementById("alerta2");
+    if (n1>=100 && n1<=200){
+        resultado.innerHTML += multiplo3(n1);
+    }else{
+        resultado.innerHTML += "<br/> no está en el rango";
+    }
+    document.getElementById("multiplo").focus();
+    document.getElementById("multiplo").select();
+});
+
+///////////// Funciones para el ejercicio 5 //////////////////
+function revisaSuma (a2){
+    if ((a2[0]==a2[1]+a2[2])||(a2[1]==a2[0]+a2[2])||(a2[2]==a2[0]+a2[1])){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let btnCalculaSuma = document.getElementById("btnCalculaSuma");
+btnCalculaSuma.addEventListener("click", function (event){
+    event.preventDefault();  //le quita el funcionamiento que tenía el boton por default
+    let n1 = parseInt(document.getElementById("num1").value);
+    let n2 = parseInt(document.getElementById("num2").value);
+    let n3 = parseInt(document.getElementById("num3").value);
+    let a2 = [n1,n2,n3];
+    let resultado = document.getElementById("alerta");
+    let mensaje = revisaSuma(a2);
+
+    switch (mensaje) {
+        case true:
+            resultado.innerHTML += "<br>Uno de los numeros es la suma de los otros 2";
+            break;
+        case false:
+            resultado.innerHTML += "<br>ningún numero es la suma de los otros 2";
+            break;
+        default:
+            break;
+    }
+});
+
+///////////// Funcion para el ejercicio 6 //////////////////
+function paridad(numero) {
+    if (numero %2 == 0){
+        return "<br>Es par";
+    }else{
+        return "<br>Es impar";
+    }
+    }
+
+let btnParidad = document.getElementById("btnParidad");
+btnParidad.addEventListener("click", function (event){
+    event.preventDefault();  //le quita el funcionamiento que tenía el boton por default
+    let n1 = parseInt(document.getElementById("parImpar").value);
+    let resultado = document.getElementById("alerta3");
+    resultado.innerHTML+= paridad(n1);
+
+    document.getElementById("parImpar").focus();
+    document.getElementById("parImpar").select();
+});
